5ee326a14eb6e3636c80d8213dfc791d472879b4
Toshikazu Matsumura <matutosi@gmail.com>
2025-03-08 16:14:31 +0900

    start april / January

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ settings = st.sidebar
 
 def create_calender():
     path_calendar = vercal.calendar_weekly_vertical(year, 
+        start_april     = start_april,
         font_path       = font_path, 
         hour_start      = hour_start, 
         hour_end        = hour_end,
@@ -27,6 +28,7 @@ def create_calender():
 with settings:
     st.button('Create Calendar', on_click=create_calender)
     year                 = st.number_input('Year:', value=datetime.datetime.now().year, step=1)
+    start_april          = st.checkbox('Starts with April', value=True)
     hour_start, hour_end = st.slider("Range in a day", min_value=0, max_value=24, value=(6, 24), step=1)
     starts_with_mon      = st.checkbox('Starts with Monday', value=True)
     adjust_left          = st.checkbox('Adjust left'       , value=True)
